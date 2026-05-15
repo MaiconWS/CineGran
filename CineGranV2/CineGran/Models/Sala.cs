0c15d19a0688fcb6eb2e107e5b49a15b9fca296b
MaiconMW <maicon.w312@outlook.com>
2026-05-15 20:51:28 -0300

    Finalizado class Sala

--- CineGranV2/CineGran/Models/Sala.cs
+++ CineGranV2/CineGran/Models/Sala.cs
@@ -1,6 +1,75 @@
+using System.Security.Cryptography.X509Certificates;
+
 namespace Cinema.Models;
 
 public class Sala
 {
-    
+    //Atributos da classe Sala
+    private int qtdeFileiras;
+    private int qtdePoltronasPorFileira;
+
+    //Construtor para criar um objeto Sala
+    public Sala(int fileiras, int qtdePoltronasPorFileira)
+    {
+        this.qtdeFileiras = fileiras;
+        this.qtdePoltronasPorFileira = qtdePoltronasPorFileira;
+    }
+
+    public int QtdeFileiras => qtdeFileiras;
+    public int QtdePoltronasPorFileira => qtdePoltronasPorFileira;
+
+    // Métodos de mapeamento (sem controle de ocupação)
+    public char GetLetraFileira(int indiceFileira)
+    {
+        return (char)('A' + indiceFileira);
+    }
+
+    public int GetIndiceFileira(char letraFileira)
+    {
+        letraFileira = char.ToUpper(letraFileira);
+        return letraFileira - 'A';
+    }
+
+    //Valida se a poltrona existe fisicamente na sala
+    public bool PoltronaExiste(char fileira, int poltrona)
+    {
+        int fileiraIndex = GetIndiceFileira(fileira);
+        return fileiraIndex >= 0 && fileiraIndex < qtdeFileiras && 
+            poltrona > 0 && poltrona <= qtdePoltronasPorFileira;
+    }
+
+    public void ExibirMapa(bool[,]? ocupacao)
+    {
+        Console.WriteLine("\n=== Mapa de Assentos ===");
+        Console.Write("    ");
+        for (int p = 0; p < qtdePoltronasPorFileira; p++)
+        {
+            Console.Write($" {p + 1,2} ");
+        }
+        Console.WriteLine();
+
+        for (int i = 0; i < qtdeFileiras; i++)
+        {
+            Console.Write($" {GetLetraFileira(i)}  ");
+            for (int j = 0; j < qtdePoltronasPorFileira; j++)
+            {
+                if(ocupacao != null && ocupacao[i, j])
+                {
+                    Console.Write(" [X] ");
+                }
+                else
+                {
+                    Console.Write(" [ ] ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Legenda: [ ] Livre,  [X] Ocupada\n");
+        }
+
+        public void ExibirMapa() // Mudança de assinatura para exibir mapa sem controle de ocupação
+        {
+            ExibirMapa(null);
+        }
+
+    }
 }
